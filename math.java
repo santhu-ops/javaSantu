e61cd1f24b1bd9dda749579295b1bdd2088e54e0
santhu-ops <kumarsanthosh26007@gmail.com>
2024-12-09 12:22:00 +0530

    Add files via upload

---
+++ math.java
@@ -0,0 +1,12 @@
+public class Math {
+    public static void main(String args[]){
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1+num2;
+        int mul = num1*num2;
+        double div =num1/num2;
+        System.err.println("the sum of "+ num1 + " and " + num2 +" is " + sum);
+        System.out.println("the product of " + num1 + " and " + num2 +" is " + mul);
+        System.out.println("the result of " + num1 + " / " + num2 + " is " + div);
+    }
+}
